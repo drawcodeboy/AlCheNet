bb56b19b11b1a71d86c95468ab8c11f274514bbd
drawcodeboy <dobylive01@gmail.com>
2025-04-29 15:31:32 +0000

    [Add] default files

--- test.py
+++ test.py
@@ -1,21 +1,59 @@
-import mne
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import time
+import torch
 
-root = "data/open-nuro-dataset/dataset/sub-001/eeg/sub-001_task-eyesclosed_eeg.set"
+import argparse
+import time, sys, os, yaml
 
-raw = mne.io.read_raw_eeglab(root, preload=True, verbose=False)
+from utils import evaluate
+from models import load_model
+from datasets import load_dataset
 
-print(raw.info['ch_names'])
+def add_args_parser():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--config', type=str)
+    
+    return parser
 
-raw.filter(l_freq=1.0, h_freq=40.0)
+def main(cfg):
+    print(f"=====================[{cfg['expr']}]=====================")
+    
+    # Device Setting
+    device = None
+    if cfg['device'] != 'cpu' and torch.cuda.is_available():
+        device = cfg['device']
+    else: 
+        device = 'cpu'
+    print(f"device: {device}")
+    
+    # Hyperparameter Settings
+    hp_cfg = cfg['hyperparameters']
+    
+    # Load Dataset
+    data_cfg = cfg['data']
+    test_ds = load_dataset(data_cfg)
+    test_dl = torch.utils.data.DataLoader(test_ds,
+                                          batch_size=hp_cfg['batch_size'])
+    print(f"Load Dataset {data_cfg['dataset']}")
+    
+    # Load Model
+    model_cfg = cfg['model']
+    model = load_model(model_cfg).to(device)
+    ckpt = torch.load(os.path.join(cfg['save_dir'], cfg['weights_file_name']),
+                      map_location=device, weights_only=False)
+    model.load_state_dict(ckpt['model'])
+    
+    start_time = int(time.time())
+    result = evaluate(model, test_dl, device)
+    test_time = int(time.time() - start_time)
+    print(f"Test Time: {test_time//60:02d}m {test_time%60:02d}s")
+    
+    for key, value in result.items():
+        print(f"{key}: {value:.4f}")
 
-metadata = pd.read_csv("data/open-nuro-dataset/dataset/participants.tsv", sep='\t')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Test', parents=[add_args_parser()])
+    args = parser.parse_args()
 
-raw.plot()
-data = raw._data
-print(data.shape)
-
-plt.savefig('./test.png')
+    with open(f'configs/test/{args.config}.yaml') as f:
+        cfg = yaml.full_load(f)
+    
+    main(cfg)
